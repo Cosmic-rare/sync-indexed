0e6df5fb85d81b71e4578393ef692c83903a25b9
Cosmic-rare <ysolakey1116@gmail.com>
2022-08-16 12:18:17 +0900

    Update: sort by create,update,+,-

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import db from "./db";
 import { useLiveQuery } from "dexie-react-hooks";
 import Inputs from "./components/Inputs";
 import TaskItem from "./components/TaskItem";
+import Order from "./components/Order";
 
 const App = () => {
-  const tasks = useLiveQuery(() => db.tasks.orderBy("_createdAt").toArray());
+  const [order, setOrder] = useState("create+");
+  const tasks = useLiveQuery(
+    () =>
+      order.slice(-1) === "-"
+        ? db.tasks
+            .orderBy(
+              order.slice(0, -1) === "update" ? "_updatedAt" : "_createdAt"
+            )
+            .toArray()
+        : db.tasks
+            .orderBy(
+              order.slice(0, -1) === "update" ? "_updatedAt" : "_createdAt"
+            )
+            .reverse()
+            .toArray(),
+    [order]
+  );
 
   return (
     <div>
       <Inputs />
+      <Order order={order} setOrder={setOrder} />
       <ul style={{ listStyle: "none" }}>
         {tasks
           ? tasks.map((val, index) => {
-            return <TaskItem task={val} key={val._id} />;
-          })
+              return <TaskItem task={val} key={val._id} />;
+            })
           : null}
       </ul>
     </div>
